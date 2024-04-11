6246dd19db57e97d76fd68b879fe22dc9f6dc56c
Orvius <Okunev.Aleksander@mail.ru>
2024-04-12 01:24:29 +0300

    solve functions task

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,3 +1,17 @@
 // Напишите функцию, которая генерирует
 // идентификатор строки (указанной длины) из
 // случайных символов.
+
+function generateRandomId(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomId = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomId += characters[randomIndex];
+  }
+
+  return randomId;
+}
+
+generateRandomId(5);
