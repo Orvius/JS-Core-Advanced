f6697eee7e3866370b217304256958e74cde7be1
Orvius <Okunev.Aleksander@mail.ru>
2024-04-12 21:21:13 +0300

    solve classes task

--- Classes/classes.js
+++ Classes/classes.js
@@ -8,3 +8,31 @@
 // включить цену книги. Создайте экземпляр
 // класса «Электронная книга» и отобразите его
 // сведения
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  displayInfo() {
+    return `Название книги: ${this.title}, автор: ${this.author}, год публикации: ${this.year}`;
+  }
+}
+
+class Ebook extends Book {
+  constructor(title, author, year, price) {
+    super(title, author, year);
+    this.price = price;
+  }
+
+  displayInfo() {
+    super.displayInfo();
+    return `${super.displayInfo()}, цена: ${this.price}$`;
+  }
+}
+
+let ebook = new Ebook("1984", "Джордж Оруэлл", 1949, 10);
+
+console.log(ebook.displayInfo());
