a8e2e4d91635defe6d881477ce3571e486626600
Orvius <Okunev.Aleksander@mail.ru>
2024-04-15 23:44:47 +0300

    solve error handling task

--- Error Handling/error_handling.js
+++ Error Handling/error_handling.js
@@ -1,3 +1,17 @@
 // Напишите функцию, которая принимает
 // массив в качестве параметра и выдает
 // пользовательскую ошибку, если массив пуст.
+
+function checkEmptyArray(arr) {
+  if (arr.length === 0) {
+    throw new Error("Массив пуст!");
+  }
+}
+
+try {
+  const myArray = [];
+  checkEmptyArray(myArray);
+  console.log("Массив не пустой.");
+} catch (error) {
+  console.error(error.message);
+}
