d537602d5341d6b84c1e20726a11329d4eb5c808
Orvius <Okunev.Aleksander@mail.ru>
2024-04-17 00:01:24 +0300

    solve promise task

--- Promise/promise.js
+++ Promise/promise.js
@@ -1,3 +1,38 @@
 // Напишите функцию, которая конвертирует
 // функцию, основанную на callbacks, в
 // функцию, основанную на Promises.
+
+function callbackToPromise(func) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      func(...args, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+}
+
+// Пример
+function asyncFunctionWithCallback(input, callback) {
+  setTimeout(() => {
+    if (input === "success") {
+      callback(null, "Успех");
+    } else {
+      callback(new Error("Ошибка"));
+    }
+  }, 1000);
+}
+
+const asyncFunctionWithPromise = callbackToPromise(asyncFunctionWithCallback);
+
+asyncFunctionWithPromise("success")
+  .then((result) => {
+    console.log("Результат:", result);
+  })
+  .catch((error) => {
+    console.error("Ошибка:", error);
+  });
