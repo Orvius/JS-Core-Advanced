dcc622d9d039f923312dd610a1d2c16e84e0638c
Orvius <Okunev.Aleksander@mail.ru>
2024-04-17 18:37:00 +0300

    solve async_await and event_loop tasks

--- Async await/async_await.js
+++ Async await/async_await.js
@@ -5,3 +5,27 @@
 // принимать идентификатор пользователя в
 // качестве аргумента и возвращать объект с
 // данными о пользователе.
+
+async function fetchUserData(userId) {
+    try {
+        const response = await fetch(`/api/users/${userId}`);
+        if (!response.ok) {
+            throw new Error('Ошибка при загрузке данных о пользователе');
+        }
+        const userData = await response.json();
+        return userData;
+    } catch (error) {
+        console.error('Произошла ошибка:', error);
+        throw error;
+    }
+}
+
+// Пример
+const userId = '123';
+fetchUserData(userId)
+    .then(userData => {
+        console.log('Данные о пользователе:', userData);
+    })
+    .catch(error => {
+        console.error('Ошибка при загрузке данных о пользователе:', error);
+    });
--- Event Loop/event_loop.js
+++ Event Loop/event_loop.js
@@ -2,3 +2,18 @@
 // 10 через секунду каждый раз.
 // Ограничения: setTimeout и new Promise() мы
 // можно вызывать только один раз.
+
+function delayedOutput(number) {
+    return new Promise(function(resolve) {
+      setTimeout(() => resolve(number), 1000);
+    });
+  }
+  
+  async function printNumbers() {
+    for (let i = 1; i <= 10; i++) {
+      let number = await delayedOutput(i);
+      console.log(number);
+    }
+  }
+  
+  printNumbers();
